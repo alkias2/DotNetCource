340c6390ef9effaf71d659d060d05d5a948968d3
Sotiris Zelos <szelos@intellisoft.gr>
2024-11-18 11:01:05 +0200

    DbContext help files

---
+++ Data/SeedData.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SpentSmart.Data
+{
+    public static class SeedData
+    {
+        public static void EnsurePopulated(IApplicationBuilder app) {
+            StoreDbContext context = app.ApplicationServices
+                .CreateScope()
+                .ServiceProvider
+                .GetRequiredService<StoreDbContext>();
+
+            if (context.Database.GetPendingMigrations().Any()) {
+                context.Database.Migrate();
+            }
+        }
+    }
+}
---
+++ Data/StoreDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SpentSmart.Models;
+
+namespace SpentSmart.Data {
+    public class StoreDbContext: DbContext {
+        public StoreDbContext(
+            DbContextOptions<StoreDbContext> options
+        ) : base(options) { }
+
+        public DbSet<Expense> Customer => Set<Expense>();
+    }
+}
